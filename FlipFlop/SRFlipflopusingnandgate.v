c1f0dc913d9d6e72085b66dcf97897210b0c0291
Xokingvk <mrvishnuvardha08@gmail.com>
2026-08-16 14:44:26 +0530

    srflipflopusingnandgate added

---
+++ FlipFlop/SRFlipflopusingnandgate.v
@@ -0,0 +1,33 @@
+module srflipflopgatelevel(input s,r,clk, output q,qbar);
+    wire w1,w2;
+    nand(w1,s,clk);
+    nand(w2,r,clk);
+    nand(q,w1,qbar);
+    nand(qbar,w2,q);
+endmodule
+
+module srflipflopbehaviourlevel(input s,r,clk, output reg q,qbar);
+    always @(posedge clk) begin
+            case ({s,r})
+                2'b11: {q,qbar} <= 2'bxx; // invalid
+                2'b01: {q,qbar} <= 2'b01; //reset
+                2'b10: {q,qbar} <= 2'b10; //set
+                2'b00: {q,qbar} <= {q,qbar}; //hold
+                default: {q,qbar} <= 2'bxx;
+            endcase
+    end
+endmodule
+
+module tb;
+    reg s,r,clk;
+    wire q,qbar;
+    integer m;
+    srflipflopbehaviourlevel s1(s,r,clk,q,qbar);
+    initial begin
+        $monitor("%b     %b     %b     %b     %b",clk,s,r,q,qbar);
+        for (m = 7;m>=0;m=m-1) begin
+            {s,r,clk} = m;
+            #1;
+        end    
+    end
+endmodule
